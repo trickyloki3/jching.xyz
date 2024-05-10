dda9335ccb56c1d2fd539c53ab5eb1f75543b0a5
trickyloki3 <tricky.loki3@gmail.com>
2024-05-09 23:21:18 -0400

    Updated post to sort by date in descending order.

--- features/post.py
+++ features/post.py
@@ -9,6 +9,9 @@ def get_post_list(post_dir):
         if post_file.endswith('.yaml'):
             post_list.append(get_post(os.path.join(post_dir, post_file)))
 
+    # sort post list by date in descending order
+    post_list.sort(key = lambda x: x['date'], reverse = True)
+
     return post_list
 
 def get_post(post_file):
