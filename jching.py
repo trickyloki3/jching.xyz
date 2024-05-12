b8208954cf8ec75274bfb0a116573dcfab28b6fa
trickyloki3 <tricky.loki3@gmail.com>
2024-05-11 20:16:17 -0400

    Updated config unpacking into index.html.

--- jching.py
+++ jching.py
@@ -9,14 +9,13 @@ app = flask.Flask(
 from features import config, post
 
 config = config.get_config('config.yaml')
-post_list = post.get_post_list(config['post_dir'])
+post_list = post.get_post_list(config['post_folder'])
 
 @app.route("/")
 def index():
     return flask.render_template(
         'index.html',
-        color_palette = config['color_palette'],
-        tag_color = config['tag_color'],
+        **config,
         post_list = post_list
     )
 
@@ -24,7 +23,6 @@ def index():
 def filter_by_meta(meta):
     return flask.render_template(
         'index.html',
-        color_palette = config['color_palette'],
-        tag_color = config['tag_color'],
+        **config,
         post_list = list(filter(lambda x: x['meta'] == meta, post_list))
     )
